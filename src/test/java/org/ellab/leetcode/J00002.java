a128a7fe73190ce5b597078f9cc69711f3fea588
Angus <angusdev@gmail.com>
2022-03-06 19:46:50 +0800

    Update javadoc

--- src/test/java/org/ellab/leetcode/J00002.java
+++ src/test/java/org/ellab/leetcode/J00002.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 /**
+ * 2 - Add Two Numbers.
+ * 
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
  * order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
  * 
